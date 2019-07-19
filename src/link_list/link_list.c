4a6b4dd8a3ca8c063902558f8f4c084469364aa3
wangheng <673602165@qq.com>
2019-07-19 14:12:57 +0800

    restructure

---
+++ src/link_list/link_list.c
@@ -0,0 +1,18 @@
+//
+// Created by wangheng on 7/19/19.
+//
+#include "link_list.h"
+
+void create_link_list1(LinkList *list) {
+
+}
+
+void ll_insert_element(LinkList *list, Element_t number) {
+
+}
+
+void ll_is_valid(LinkList *list) {
+
+}
+
+
---
+++ src/main2.c
@@ -0,0 +1,10 @@
+//
+// Created by wangheng on 7/19/19.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("hello world\n\n");
+    return 0;
+}
