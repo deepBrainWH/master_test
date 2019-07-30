789bfc7ed773a12a1f25c434404bf87280bf9a38
wangheng <673602165@qq.com>
2019-07-30 20:05:14 +0800

    update

--- src/stack/stack_queue.c
+++ src/stack/stack_queue.c
@@ -5,7 +5,7 @@
 #include "stack_queue.h"
 
 void init_stack(Stack *s) {
-    s->a = malloc(sizeof(stack_element_t)*MAX_CAPACITY);
+    s->a = (stack_element_t *)malloc(sizeof(stack_element_t)*MAX_CAPACITY);
     s->top = -1;
 }
 
@@ -34,4 +34,60 @@ void free_stack(Stack *stack) {
     free(stack->a);
     free(stack);
 }
+
+CircleQueue *init_circle_queue() {
+    CircleQueue* queue = (CircleQueue*)malloc(sizeof(CircleQueue));
+    queue->elements = (queue_element_t*)malloc(sizeof(queue_element_t)*MAX_CAPACITY);
+    queue->front = 0;
+    queue->rear = 0;
+    queue->tag = 0;
+    return queue;
+}
+
+int is_queue_empty(CircleQueue *queue) {
+    if (queue->tag == 0){
+        return 1;
+    }
+    return 0;
+}
+
+void en_queue(CircleQueue *queue, queue_element_t element) {
+    is_valid_of_circle_queue(queue);
+    if(queue->tag == MAX_CAPACITY){
+        printf("\033[1;31mThe queue has already fulled\033[0m\n");
+        return;
+    }
+    queue->elements[queue->rear] = element;
+    queue->rear = (queue->rear+1) % MAX_CAPACITY;
+    queue->tag ++;
+}
+
+queue_element_t de_queue(CircleQueue *queue) {
+    is_valid_of_circle_queue(queue);
+    if(queue->tag == 0){
+        printf("\033[0;31mThis queue is empty!\033[0m\n");
+        return (queue_element_t) NULL;
+    }else{
+        queue_element_t t = queue->elements[queue->front];
+        queue->front = (queue->front+1) % MAX_CAPACITY;
+        queue->tag--;
+        return t;
+    }
+}
+
+void is_valid_of_circle_queue(CircleQueue *queue) {
+    if(queue==NULL){
+        printf("\033[1;31mThe queue is NULL\033[0m\n");
+        exit(-1);
+    }
+}
+
+void destroy_circle_queue(CircleQueue *queue) {
+    if(queue != NULL){
+        free(queue->elements);
+        free(queue);
+    }
+}
+
+
 
--- src/stack/stack_queue.h
+++ src/stack/stack_queue.h
@@ -8,14 +8,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_CAPACITY 100
+#define MAX_CAPACITY 5
 
 typedef struct Stack Stack;
+typedef struct circle_queue CircleQueue;
 typedef int stack_element_t;
+typedef int queue_element_t;
 
 struct Stack {
     stack_element_t *a;
     int top;
+};
+
+struct circle_queue{
+    queue_element_t* elements;
+    int tag;
+    int front;
+    int rear;
 };
 
 
@@ -29,5 +38,17 @@ stack_element_t pop_stack(Stack* stack);
 int is_empty_stack(Stack* stack);
 
 void free_stack(Stack* stack);
+
+CircleQueue* init_circle_queue();
+
+int is_queue_empty(CircleQueue* queue);
+
+void en_queue(CircleQueue* queue, queue_element_t element);
+
+queue_element_t de_queue(CircleQueue* queue);
+
+void is_valid_of_circle_queue(CircleQueue* queue);
+
+void destroy_circle_queue(CircleQueue* queue);
 
 #endif //MASTER_TEST_STACK_QUEUE_H
