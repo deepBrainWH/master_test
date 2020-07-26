6ca98fb3195acb471819db65f2ce22b1a7a7eb20
deepBrainWH <673602165@qq.com>
2020-07-26 10:57:41 +0800

    update

--- src/imgproc/mat_mask_operation.cc
+++ src/imgproc/mat_mask_operation.cc
@@ -10,14 +10,28 @@
 using namespace std;
 using namespace cv;
 
+
 const string fileName = "/home/wangheng/workspace/CLionProjects/master_test/data/ziqi.jpg";
 
-int main(){
+inline void help() {
+    cout << endl
+         << "This program shows how to filter image with mask"
+         << endl;
+    cout<<__FILE__<<endl;
+}
+
+void sharpen(const Mat& inputImage, Mat& result) {
+
+}
+
+int main() {
+    help();
     Mat image = imread(fileName, IMREAD_ANYCOLOR);
-    namedWindow("input image",WINDOW_FREERATIO);
+    namedWindow("input image", WINDOW_FREERATIO);
     imshow("input image", image);
     waitKey(0);
     destroyAllWindows();
+    help();
     return 0;
 }
 
