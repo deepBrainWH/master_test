d6bdd1f75c309d1f23e455577b50cbd9a19c2183
deepBrainWH <673602165@qq.com>
2020-07-26 11:28:36 +0800

    update

--- src/imgproc/mat_mask_operation.cc
+++ src/imgproc/mat_mask_operation.cc
@@ -17,18 +17,48 @@ inline void help() {
     cout << endl
          << "This program shows how to filter image with mask"
          << endl;
-    cout<<__FILE__<<endl;
+    cout << __FILE__ << endl;
 }
 
 void sharpen(const Mat& inputImage, Mat& result) {
-
+    CV_Assert(inputImage.depth() == CV_8U);
+    const int channels = inputImage.channels();
+    result.create(inputImage.size(), inputImage.type());
+    for (int j = 1; j < inputImage.rows - 1; j++) {
+        const uchar* previous = inputImage.ptr<uchar>(j - 1);
+        const uchar* current = inputImage.ptr<uchar>(j);
+        const uchar* next = inputImage.ptr<uchar>(j + 1);
+        uchar* output = result.ptr<uchar>(j);
+        for (int i = channels; i < channels * (inputImage.cols - 1); ++i) {
+            *output = saturate_cast<uchar>(5 * current[i] -
+                                           current[i - channels] -
+                                           current[i + channels] -
+                                           previous[i] - next[i]);
+            output++;
+        }
+    }
+    result.row(0).setTo(Scalar(0));
+    result.row(result.rows - 1).setTo(Scalar(0));
+    result.col(0).setTo(Scalar(0));
+    result.col(result.cols - 1).setTo(Scalar(0));
 }
 
 int main() {
     help();
     Mat image = imread(fileName, IMREAD_ANYCOLOR);
+    Mat dst0, dst1;
     namedWindow("input image", WINDOW_FREERATIO);
+    namedWindow("output image", WINDOW_FREERATIO);
+    namedWindow("dst1", WINDOW_FREERATIO);
     imshow("input image", image);
+    sharpen(image,dst0);
+    Mat kernel = (Mat_<char>(3, 3) <<
+                                   0, -1, 0,
+            -1, 5, -1,
+            0, -1, 0);
+    filter2D(image, dst1, image.depth(), kernel);
+    imshow("output image", dst0);
+    imshow("dst1", dst1);
     waitKey(0);
     destroyAllWindows();
     help();
